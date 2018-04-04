11c2daf5cf1a38cf38f29c54ca69c26abe79de23
paparong <cwrooong@163.com>
2018-04-04 16:55:05 +0800

    Capture_Stats
    gather system performence statistics

--- Capture_Stats.sh
+++ Capture_Stats.sh
@@ -13,4 +13,11 @@ TIME=`date +%k:%M:%S`
 USERS=`uptime | sed 's/users.*$//' | gawk '{print $NF}'`
 LOAD=`uptiem | gawk '{print $NF}'`
 #
-FREE=`vmstat 1 2 | sed `
+FREE=`vmstat 1 2 | sed -n '/[0-9]/p' | sed -n '2p' | gawk '{print $4}'`
+IDLE=`vmstat 1 2 | sed -n '/[0-9]/p' | sed -n '2p' | gawk '{print $15}'`
+#
+#####################################################
+#send statistics to report file
+#
+echo "$DATE.$TIME.$USERS.$LOAD.$FREE.$IDLE" >> $REPORT_FILE
+#
