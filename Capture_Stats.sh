3322446d7a3ee9ef09ccfc7fb2b9c4f2f2b64a86
paparong <cwrooong@163.com>
2018-04-04 16:07:06 +0800

    Create Capture_Stats.sh

---
+++ Capture_Stats.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#Capture_Stats - gather system performence statistics
+#####################################################
+#set variable
+REPORT_FILE=/home/user/Documents/capstat.csv
+DATE=`date +%Y%m%d`
+TIME=`date +%k:%M:%S`
+#
+#####################################################
+#gather 
