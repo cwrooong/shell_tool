95acc55f6e8c72af3ae9bc590399182ce9c8c12b
paparong <cwrooong@163.com>
2018-04-10 18:02:03 +0800

    Update Capture_Stats.sh

--- Capture_Stats.sh
+++ Capture_Stats.sh
@@ -10,7 +10,7 @@ TIME=`date +%k:%M:%S`
 #####################################################
 #gather performence statistics
 #
-USERS=`uptime | sed 's/users.*$//' | gawk '{print $NF}'` ##user number
+USERS=`uptime | sed 's/users.*$//' | gawk '{print $NF}'` ##user number; if only have one user,then sed 's/user.*$//'
 LOAD=`uptime | gawk '{print $NF}'`                       ##last 15 min average 
 #
 FREE=`vmstat 1 2 | sed -n '/[0-9]/p' | sed -n '2p' | gawk '{print $4}'`
